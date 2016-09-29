3367d971c4d822fd5e3e51e38bce30c881beac27
Jon Borrelli <borrejj@gmail.com>
2016-09-28 23:05:42 -0400

    reset scripts

---
+++ RScripts/funcs.R
@@ -0,0 +1,36 @@
+library(igraph)
+library(NetIndices)
+library(plyr)
+library(ggplot2)
+library(reshape2)
+library(parallel)
+library(doSNOW)
+library(rend)
+library(rootSolve)
+
+
+n.vals <- function(S,C){
+  niche<-runif(S,0,1)
+  r<-rbeta(S,1,((1/(2*C))-1))*niche
+  
+  ci <- runif(S, r/2, niche)
+  
+  return(cbind(niche = niche, ci = ci, r = r))
+}
+
+n.mat <- function(nv){
+  S <- nrow(nv)
+  new.mat<-matrix(0,nrow=S,ncol=S)
+  
+  for(i in 1:S){
+    
+    for(j in 1:S){
+      if(nv[j,1]>(nv[i,2]-(.5*nv[i,3])) && nv[j,1]<(nv[i,2]+.5*nv[i,3])){
+        new.mat[j,i]<-1
+      }
+    }
+  }
+  
+  #new.mat<-new.mat[order(apply(new.mat,2,sum)),order(apply(new.mat,2,sum))]
+  return(new.mat)
+}
---
+++ RScripts/niche-START.R
@@ -0,0 +1,34 @@
+# source()
+
+t0 <- Sys.time()
+
+n1 <- lapply(1:500, function(x) n.vals(60, C = .1))
+n2m <- lapply(n1, n.mat)
+xiL <- lapply(n2m, function(x){(((10^2)^TrophInd(x)$TL)/100)^-.25*.314})
+riL <- lapply(n2m, function(x){(colSums(x) == 0)})
+pars <- lapply(1:500, function(x){list(K = 1, x.i = xiL[[x]], yij = 8, eij = .85, xpar = .2, 
+                                        B.o = 0.5, r.i = riL[[x]], A = n2m[[x]], G.i = Gi,
+                                        FR = Fij)})
+
+state1 <- lapply(1:500, function(x) runif(60, .5, 1))
+
+
+filepath.data <- "D:/jjborrelli/invadr/dynDATA/"
+
+cl <- makeCluster(detectCores()-1)
+clusterExport(cl, c("n2m", "pars", "state1", "filepath.data"))
+registerDoSNOW(cl)
+system.time(
+  randos <- foreach(i = 1:500, .packages = c("rend", "deSolve")) %dopar% {
+    
+    dyn <- ode(y = state1[[i]], times = 1:4000, func = CRmod, parms = pars[[i]], 
+               events = list(func = ext1, time = 1:4000))
+    
+    write.csv(dyn, file = paste0(filepath.data, "web-", i, ".csv", collapse = ""))
+    return(dyn)
+  }
+)
+stopCluster(cl)
+
+t1 <- Sys.time()
+t1-t0
